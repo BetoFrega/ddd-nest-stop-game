0a161bd18a092252386275562b5c68b47f09ba3e
R.Frega <robertofrega@gmail.com>
2021-05-02 02:32:24 -0300

    test: refatorando testes

---
+++ api/test/Autenticacao.e2e-spec.ts
@@ -0,0 +1,88 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { IUsuáriosRepository } from '../src/app/infrastructure/repositories/UsuáriosRepository';
+import { PrismaService } from '../src/app/infrastructure/repositories/Prisma.service';
+
+describe('Autenticação', () => {
+  let app: INestApplication;
+  let userRepository: IUsuáriosRepository;
+  let prismaService: PrismaService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    userRepository = moduleFixture.get<IUsuáriosRepository>(
+      'IUsuáriosRepository',
+    );
+    prismaService = moduleFixture.get<PrismaService>(PrismaService);
+    await app.init();
+    await prismaService.credencial.deleteMany({}).then(() => {
+      prismaService.usuario.deleteMany({});
+    });
+  });
+
+  afterEach(async () => {
+    await prismaService.credencial.deleteMany({}).then(() => {
+      prismaService.usuario.deleteMany({});
+    });
+  });
+
+  describe('registro', () => {
+    it('Retorna usuário quando registra', async () => {
+      expect.assertions(1);
+      return request(app.getHttpServer())
+        .post('/auth/cadastro')
+        .send({
+          email: 'fulane.silveira@gmail.com',
+          senha: 'senha de fulane',
+        })
+        .expect(201)
+        .expect((res) => {
+          expect(res.body).toEqual(
+            expect.objectContaining({
+              email: 'fulane.silveira@gmail.com',
+              uuid: expect.stringMatching(/\S+/),
+            }),
+          );
+        });
+    });
+
+    it('Salva usuário no banco', () => {
+      expect.assertions(1);
+      return request(app.getHttpServer())
+        .post('/auth/cadastro')
+        .send({
+          email: 'fulane.silveira@gmail.com',
+          senha: 'senha de fulane',
+        })
+        .expect(201)
+        .expect(async (res) => {
+          const savedUser = await userRepository.emailExiste(res.body.email);
+          return expect(savedUser).toEqual(true);
+        });
+    });
+
+    it('Retorna JWT quando registra', async () => {
+      expect.assertions(1);
+      return request(app.getHttpServer())
+        .post('/auth/cadastro')
+        .send({
+          email: 'fulane.silveira@gmail.com',
+          senha: 'senha de fulane',
+        })
+        .expect(201)
+        .expect((res) => {
+          expect(res.body).toEqual(
+            expect.objectContaining({
+              jwt: expect.stringMatching(/\S+/),
+            }),
+          );
+        });
+    });
+  });
+});
